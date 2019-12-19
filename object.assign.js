7f591ee13bc4680a20b036bd708c1fdb421ad89e
Masao Kitamura <masaok@gmail.com>
2019-12-19 01:23:44 -0800

    init

---
+++ object.assign.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+
+const returnedTarget = Object.assign(target, source);
+
+console.log(target);
+// expected output: Object { a: 1, b: 4, c: 5 }
+
+console.log(returnedTarget);
+// expected output: Object { a: 1, b: 4, c: 5 }
+
